eddddd06a3b7e6c0b90b6696cddacb3470be1b54
elouannh <elouannhosta01@gmail.com>
2024-01-30 15:59:55 +0100

    Configured tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterButtonComponent } from './counter-button/counter-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterButtonComponent
   ],
   imports: [
     BrowserModule,

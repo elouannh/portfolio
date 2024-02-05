877b3ceb00df3bf9dd3289986c981c71c6483cd9
elouannh <elouannhosta01@gmail.com>
2024-02-05 12:58:32 +0100

    Updated the main structure: component styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title: string = 'portfolio';
+  public toggled: boolean | 'unset' = 'unset';
+  public backgroundStyle: string = 'var(--black)';
+
+  public enableLight(event: MouseEvent): void {
+    if (!this.toggled) {
+      this.toggled = true;
+      document.body.classList.add('cursor-light-on');
+      document.body.classList.remove('cursor-light-off');
+    }
+    const x: number = event.pageX;
+    const y: number = event.pageY;
+    this.backgroundStyle = `radial-gradient(circle at ${x}px ${y}px, rgba(150, 150, 155, 0.8), black 2000px)`;
+  }
+
+  public disableLight(): void {
+    document.body.classList.add('cursor-light-off');
+    document.body.classList.remove('cursor-light-on');
+    this.toggled = false;
+    this.backgroundStyle = `var(--black)`;
+  }
+
+  onMouseMove(event: MouseEvent): void {
+    if (this.toggled === "unset") this.enableLight(event);
+    else if (this.toggled) this.enableLight(event);
+  }
+
+  onMouseClick(event: MouseEvent): void {
+    if (this.toggled) this.disableLight();
+    else this.enableLight(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,9 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ExperiencesComponent } from './experiences/experiences.component';
 import { ProjectsComponent } from './projects/projects.component';
-import { LanguagesComponent } from './languages/languages.component';
 import { ContactComponent } from './contact/contact.component';
 import { StuffComponent } from './stuff/stuff.component';
 import { AboutMeComponent } from './about-me/about-me.component';
-import { SectionTitleComponent } from './section-title/section-title.component';
 
 @NgModule({
   declarations: [
@@ -19,11 +17,9 @@ import { SectionTitleComponent } from './section-title/section-title.component';
     HeaderComponent,
     ExperiencesComponent,
     ProjectsComponent,
-    LanguagesComponent,
     ContactComponent,
     StuffComponent,
     AboutMeComponent,
-    SectionTitleComponent,
   ],
   imports: [
     BrowserModule,
